47599b3e961585e88020be4610e777e1f5dbf209
wallaC-137 <wallace_031@hotmail.com>
2023-05-31 08:51:45 -0300

    FIX - req03: ok implementado rota GET /orders e testes relacionado a rota em questão

--- src/app.ts
+++ src/app.ts
@@ -9,4 +9,5 @@ app.use(express.json());
 
 app.use('/products', productRouter);
 app.use('/orders', orderRouter);
+app.use('/login', orderRouter);
 export default app;
--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,31 +1,35 @@
-/* eslint-disable max-lines-per-function */
 import { Order, OrderToJson } from '../types/Order';
 import ProductModel from '../database/models/product.model';
-import OrderModel from '../database/models/order.model';
-import { ServiceResponse } from '../types/Product';
+import OrderModel, { OrderSequelizeModel } from '../database/models/order.model';
+import { ServiceResponse, ServiceResponseError } from '../types/Product';
+
+const formatOrder = (order: OrderSequelizeModel[]): ServiceResponseError | null => {
+  if (order.length === 0) {
+    const responseService = { status: 'NOT_FOUND', data: { message: 'Orders not found' } };
+    return responseService as ServiceResponseError;
+  }
+  return null;
+};
 
 const findAll = async (): Promise<ServiceResponse<Order[]>> => {
-  let responseService: ServiceResponse<Order[]>;
-
   const orders = await OrderModel.findAll({ 
     include: [{
       model: ProductModel, as: 'productIds', attributes: ['id'],
     }],
   });
   
-  if (orders.length === 0) {
-    responseService = { status: 'NOT_FOUND', data: { message: 'Orders not found' } };
-    return responseService;
-  }
+  const resultFail = formatOrder(orders);
   
+  if (resultFail) return resultFail;
+
   const objOrder: OrderToJson[] = orders.map((order) => order.toJSON());
 
   const result = objOrder.map((order) => ({
     ...order,
     productIds: order.productIds.map((product) => product.id),
   }));
-  
-  responseService = {
+
+  const responseService: ServiceResponse<Order[]> = {
     status: 'SUCCESSFUL', data: result,
   };
 
